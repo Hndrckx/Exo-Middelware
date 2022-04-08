6fa13ae3383a6b6fefa8a3955e35e886f0139966
Hndrckx <90184045+Hndrckx@users.noreply.github.com>
2022-04-08 11:59:05 +0200

    Working on Dashboard

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/users', function () {
+    return view('users');
+})->middleware(['auth'])->name('users');
+
 require __DIR__.'/auth.php';
